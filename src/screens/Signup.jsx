ed8778a3f27e6c77045d97df3a4279b894a6d404
Amit Mandhana <114164809+AmitMandhana@users.noreply.github.com>
2024-10-07 23:19:34 +0530

    Update Signup.jsx

--- src/screens/Signup.jsx
+++ src/screens/Signup.jsx
@@ -20,7 +20,7 @@ export default function Signup() {
             return;
         }
         
-        const response = await fetch("http://localhost:5000/api/createuser", {
+        const response = await fetch("https://amit19-1.onrender.com/api/createuser", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
